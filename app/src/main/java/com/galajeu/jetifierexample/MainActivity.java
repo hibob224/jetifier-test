f2b24800feb2109592b3684806238b3efcc3a7a3
Jonny Macbook <contact@jmiller.xyz>
2019-01-30 23:14:56 +0000

    Pre-Migrate

--- app/src/main/java/com/galajeu/jetifierexample/MainActivity.java
+++ app/src/main/java/com/galajeu/jetifierexample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.galajeu.jetifierexample;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.galajeu.jetifierissue2.JetifierResultReceiver;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JetifierResultReceiver receiver = new JetifierResultReceiver(new Handler());
     }
 }
